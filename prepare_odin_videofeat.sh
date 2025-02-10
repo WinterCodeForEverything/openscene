1459ca4482dd49a6d880a5e472c83d236c5ed664
WinterCodeForEverything <chao_liu@mymail.sutd.edu.sg>
2025-02-10 16:42:11 +0800

    add odin seg dinov2 feature

---
+++ prepare_odin_videofeat.sh
@@ -0,0 +1,4 @@
+ python scripts/feature_fusion/scannet_videofeat_odin.py \
+    --data_dir /mnt/ssd/liuchao/odin/scannet/frames_square_highres \
+    --output_dir /mnt/ssd/liuchao/Chat-Scene/odin_videofeats \
+    --data_mode odin
